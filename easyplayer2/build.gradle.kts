c41e41670abc59174a4237364a37655f6efaa760
heyanlin <heyanlin@tencent.com>
2024-02-06 17:08:18 +0800

    [feature] 升级compose版本

--- easyplayer2/build.gradle.kts
+++ easyplayer2/build.gradle.kts
@@ -49,19 +49,19 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.21")
 
-    val composeMaterialVersion = "1.5.4"
+    val composeMaterialVersion = "1.6.0"
     implementation("androidx.compose.material:material:$composeMaterialVersion")
     implementation("androidx.compose.material:material-icons-core:$composeMaterialVersion")
     implementation("androidx.compose.material:material-icons-extended:$composeMaterialVersion")
 
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.material3:material3:1.1.2")
 
-    val media3 = "1.2.0"
+    val media3 = "1.2.1"
     api("androidx.media3:media3-exoplayer:$media3")
     api("androidx.media3:media3-exoplayer-dash:$media3")
     api("androidx.media3:media3-ui:$media3")
